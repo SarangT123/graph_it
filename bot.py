c27d234e5ab8bb5ddcb83e62a4e127dd943a271f
Sarang Codes <72746858+SarangT123@users.noreply.github.com>
2021-09-17 04:56:18 +0000

    Added server

--- bot.py
+++ bot.py
@@ -1,9 +1,16 @@
 from discord.ext import commands
 import config
-
 bot = commands.Bot(command_prefix="!")
 bot.load_extension("cogs.graph_single")
 bot.load_extension("cogs.graph_multi")
 
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+    
 bot.run(config.TOKEN)
+app.run(host="0.0.0.0")
--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 from os import getenv
 from dotenv import load_dotenv
-import dotenv
 load_dotenv()
 
 TOKEN = getenv("TOKEN")
