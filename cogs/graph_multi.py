974c2744c2102c31f7153b35445e14ecbfa93b3a
Sarangt123 (sarangt123) <sarang.thekkedathpr@gmail.com>
2021-09-17 08:36:20 +0530

    added multi for every graph except histogram

--- cogs/graph_multi.py
+++ cogs/graph_multi.py
@@ -26,7 +26,7 @@ class graph():
         plt.title(title)
         plt.savefig('mygraph.png')
 
-    def linearGraph(self, datax, datay, title, xl, yl):
+    def linearGraph(self, datax, datay, title, xl, yl, ng):
         x = datax
         y = datay
         fig = plt.figure(figsize=(10, 5))
@@ -34,13 +34,15 @@ class graph():
         plt.rcParams['text.color'] = 'black'
         plt.rcParams['axes.labelcolor'] = 'black'
         fig.patch.set_facecolor('#fcb86a')
-        plt.plot(x, y, color='#6a9bfc')
+        for i in range(ng):
+            print(x[i], y[i])
+            plt.bar(x[i], y[i])
         plt.xlabel(xl)
         plt.ylabel(yl)
         plt.title(title)
         plt.savefig('mygraph.png')
 
-    def pieGraph(self, datax, datay, title, xl, yl):
+    def pieGraph(self, datax, datay, title, xl, yl, ng):
         x = datax
         y = datay
         fig = plt.figure(figsize=(10, 5))
@@ -48,11 +50,12 @@ class graph():
         plt.rcParams['text.color'] = 'black'
         plt.rcParams['axes.labelcolor'] = 'black'
         fig.patch.set_facecolor('#fcb86a')
-        plt.pie(y, labels=x)
+        for i in range(ng):
+            plt.pie(y[i], labels=x[i])
         plt.title(title)
         plt.savefig('mygraph.png')
 
-    def scatterGraph(self, datax, datay, title, xl, yl):
+    def scatterGraph(self, datax, datay, title, xl, yl, ng):
         x = datax
         y = datay
         fig = plt.figure(figsize=(10, 5))
@@ -60,13 +63,14 @@ class graph():
         plt.rcParams['text.color'] = 'black'
         plt.rcParams['axes.labelcolor'] = 'black'
         fig.patch.set_facecolor('#fcb86a')
-        plt.scatter(x, y, color='#6a9bfc')
+        for i in range(ng):
+            plt.scatter(x[i], y[i])
         plt.xlabel(xl)
         plt.ylabel(yl)
         plt.title(title)
         plt.savefig('mygraph.png')
 
-    def histGraph(self, datax, datay, title, xl, yl):
+    def histGraph(self, datax, datay, title, xl, yl, ng):
         x = datax
         y = datay
         fig = plt.figure(figsize=(10, 5))
@@ -74,13 +78,14 @@ class graph():
         plt.rcParams['text.color'] = 'black'
         plt.rcParams['axes.labelcolor'] = 'black'
         fig.patch.set_facecolor('#fcb86a')
-        plt.hist(x, y, histtype="bar", rwidth=0.8)
+        for i in range(ng):
+            plt.hist(x[i], y[i], histtype="bar", rwidth=0.8)
         plt.xlabel(xl)
         plt.ylabel(yl)
         plt.title(title)
         plt.savefig('mygraph.png')
 
-    def areaGraph(self, datax, datay, title, xl, yl):
+    def areaGraph(self, datax, datay, title, xl, yl, ng):
         x = datax
         y = datay
         fig = plt.figure(figsize=(10, 5))
@@ -88,7 +93,8 @@ class graph():
         plt.rcParams['text.color'] = 'black'
         plt.rcParams['axes.labelcolor'] = 'black'
         fig.patch.set_facecolor('#fcb86a')
-        plt.stackplot(x, y, color='#6a9bfc')
+        for i in range(ng):
+            plt.stackplot(x[i], y[i])
         plt.xlabel(xl)
         plt.ylabel(yl)
         plt.title(title)
@@ -103,6 +109,122 @@ class graph_multi(commands.Cog):
 
     @commands.command("bar_multi")
     async def bar_multi(self, ctx, title: str, xl: str, yl: str, *, data: str):
+        dbg = graph()
+        print(data)
+        data = data.split(" ")
+        for i in range(len(data)):
+            data[i] = data[i].split(",")
+        ng = len(data)
+        print(data, ng)
+        keys = []
+        values = []
+        for i in range(len(data)):
+            keys.append([])
+            values.append([])
+            print(i)
+
+        for i in range(len(data)):
+            for n in range(len(data[i])):
+                if n % 2 == 0:
+                    keys[i].append(data[i][n])
+                else:
+                    values[i].append(int(data[i][n]))
+        if len(values) != len(keys):
+            await ctx.send("The amount of keys and values are not the same")
+        else:
+            print(keys, values)
+            dbg.barGraph(keys, values, title, xl, yl, ng)
+            await ctx.send(file=discord.File('mygraph.png'))
+
+    @commands.command("linear_multi")
+    async def linear_multi(self, ctx, title: str, xl: str, yl: str, *, data: str):
+        dbg = graph()
+        print(data)
+        data = data.split(" ")
+        for i in range(len(data)):
+            data[i] = data[i].split(",")
+        ng = len(data)
+        print(data, ng)
+        keys = []
+        values = []
+        for i in range(len(data)):
+            keys.append([])
+            values.append([])
+            print(i)
+
+        for i in range(len(data)):
+            for n in range(len(data[i])):
+                if n % 2 == 0:
+                    keys[i].append(data[i][n])
+                else:
+                    values[i].append(int(data[i][n]))
+        if len(values) != len(keys):
+            await ctx.send("The amount of keys and values are not the same")
+        else:
+            print(keys, values)
+            dbg.barGraph(keys, values, title, xl, yl, ng)
+            await ctx.send(file=discord.File('mygraph.png'))
+
+    @commands.command("scatter_multi")
+    async def scatter_multi(self, ctx, title: str, xl: str, yl: str, *, data: str):
+        dbg = graph()
+        print(data)
+        data = data.split(" ")
+        for i in range(len(data)):
+            data[i] = data[i].split(",")
+        ng = len(data)
+        print(data, ng)
+        keys = []
+        values = []
+        for i in range(len(data)):
+            keys.append([])
+            values.append([])
+            print(i)
+
+        for i in range(len(data)):
+            for n in range(len(data[i])):
+                if n % 2 == 0:
+                    keys[i].append(data[i][n])
+                else:
+                    values[i].append(int(data[i][n]))
+        if len(values) != len(keys):
+            await ctx.send("The amount of keys and values are not the same")
+        else:
+            print(keys, values)
+            dbg.barGraph(keys, values, title, xl, yl, ng)
+            await ctx.send(file=discord.File('mygraph.png'))
+
+    @commands.command("area_multi")
+    async def area_multi(self, ctx, title: str, xl: str, yl: str, *, data: str):
+        dbg = graph()
+        print(data)
+        data = data.split(" ")
+        for i in range(len(data)):
+            data[i] = data[i].split(",")
+        ng = len(data)
+        print(data, ng)
+        keys = []
+        values = []
+        for i in range(len(data)):
+            keys.append([])
+            values.append([])
+            print(i)
+
+        for i in range(len(data)):
+            for n in range(len(data[i])):
+                if n % 2 == 0:
+                    keys[i].append(data[i][n])
+                else:
+                    values[i].append(int(data[i][n]))
+        if len(values) != len(keys):
+            await ctx.send("The amount of keys and values are not the same")
+        else:
+            print(keys, values)
+            dbg.barGraph(keys, values, title, xl, yl, ng)
+            await ctx.send(file=discord.File('mygraph.png'))
+
+    @commands.command("pie_multi")
+    async def pie_multi(self, ctx, title: str, xl: str, yl: str, *, data: str):
         dbg = graph()
         print(data)
         data = data.split(" ")
